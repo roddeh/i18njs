2de4cce9872ab6b988bc675f47975190616ced0e
Matthew Finerty <evilarceus@gmail.com>
2019-06-26 21:31:51 -0700

    Added tests and linting config for TypeScript definitions
    Also fixed and made definitions more specific

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,4 @@
-// Type definitions for roddeh-i18n v1.2.0
-// Project: https://github.com/roddeh/i18njs
-// Definitions by: Matthew Finerty <https://github.com/evilarceus>
+// TypeScript Version: 2.9
 
 /**
  * Translates the given text.
@@ -10,22 +8,36 @@
  * @param formattingOrContext Format OR context to use in translation
  * @param context Context to be used when translating
  */
-declare function roddeh_i18n(text: string | number, defaultNumOrFormatting?: number | object, numOrFormattingOrContext?: number | object | roddeh_i18n.ContextOpts, formattingOrContext?: object | roddeh_i18n.ContextOpts, context?: roddeh_i18n.ContextOpts) : string;
+declare function roddeh_i18n(text: string | number,
+    defaultNumOrFormatting?: number | roddeh_i18n.FormattingContext,
+    numOrFormattingOrContext?: number | roddeh_i18n.FormattingContext,
+    formattingOrContext?: roddeh_i18n.FormattingContext,
+    context?: roddeh_i18n.FormattingContext): string;
 
 declare namespace roddeh_i18n {
-    interface ContextOpts {
-        matches: Map<string, string | number>,
-        values: Map<string, string | Array<any>>
-    }
-    
-    interface DataOpts {
-        values?: Map<string | number, string>,
-        contexts?: Array<ContextOpts>
+    interface Values {
+        [index: string]: string | Array<string | Array<string | number | null>> | {[key: string]: string};
     }
 
-    type ExtensionFunction = (text: string, number: number, formatting: object, data: object) => string;
+    interface FormattingContext {
+        [key: string]: string;
+    }
+
+    interface ContextOpts {
+        matches: FormattingContext;
+        values: Values;
+    }
+
+    interface DataOpts {
+        values?: Values;
+        contexts?: ContextOpts[];
+    }
 
     namespace translator {
+        namespace constructor {
+            let name: string;
+        }
+
         /**
          * Translates the given text.
          * @param text Text to translate
@@ -34,61 +46,72 @@ declare namespace roddeh_i18n {
          * @param formattingOrContext Format OR context to use in translation
          * @param context Context to use in translation
          */
-        function translate(text: string | number, defaultNumOrFormatting?: number | object, numOrFormattingOrContext?: number | object | ContextOpts, formattingOrContext?: object | ContextOpts, context?: ContextOpts) : string;
-        
+        function translate(text: string | number,
+            defaultNumOrFormatting?: number | FormattingContext,
+            numOrFormattingOrContext?: number | FormattingContext,
+            formattingOrContext?: FormattingContext,
+            context?: FormattingContext): string;
+
         /**
          * Adds data (values and contexts) to i18n instance.
          * @param data The language data: { "values": { "Yes": "はい", "No": "いいえ" } }
          */
-        function add(data: DataOpts) : void;
+        function add(data: DataOpts | object): void; // object is an alternate parameter type to allow imports from JSON files
 
         /**
          * Sets the context globally. This context will be used when translating all strings unless a different context is provided when calling i18n().
          * @param key The key for the context, e.g. "gender".
          * @param value The value for the context, e.g. "female".
          */
-        function setContext(key: string, value: any) : void;
+        function setContext(key: string,
+            value: string): void;
 
         /**
          * Adds extension to i18n instance.
          * @param ext Extension to add
          */
-        function extend(ext: ExtensionFunction) : void;
+        function extend(ext: (text: string,
+            number: number,
+            formatting: FormattingContext,
+            data: Values) => string | number): void;
 
         /**
          * Clears the context for the given key.
          * @param key The key to clear
          */
-        function clearContext(key: string) : void;
+        function clearContext(key: string): void;
 
         /**
          * Destroys all translation and context data.
          */
-        function reset() : void;
+        function reset(): void;
 
         /**
          * Destroys all translation data. Useful for when you change languages.
          */
-        function resetData() : void;
+        function resetData(): void;
 
         /**
          * Destroys all context data.
          */
-        function resetContext() : void;
+        function resetContext(): void;
 
         /**
          * Translates all the keys in a hash. Useful for translating the i18n property that exists for some lovely.io packages.
          * @param hash Hash containing the strings to be translated.
          * @param context Context to be used when translating the hash values.
          */
-        function translateHash(hash: object, context: object) : object;
+        function translateHash(hash: {[key: string]: string},
+            context?: FormattingContext): object;
+
+        let data: DataOpts;
     }
 
     /**
      * Creates a new i18n instance.
      * @param data Data to add to new i18n instance
      */
-    function create(data?: DataOpts) : typeof roddeh_i18n;
+    function create(data?: DataOpts | object): typeof roddeh_i18n;
 }
 
 export = roddeh_i18n;
